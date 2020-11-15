66c265084f719cf5ef88132a1cb7cde968a6c1df
abpa <abhishek@excelsiorstudio.in>
2020-11-15 18:03:57 +0530

    add form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      short: null,
+      count: 0
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    //api call
+    this.setState({
+      short: this.state.value,
+      count: this.state.count + 1
+    })
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.handleSubmit} className="shorten-form">
+          <label>
+            <input type="url" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Shorten this" className="btn"/>
+        </form>
+        <div>
+          {
+            !this.state.short? null: <div>{this.state.count}</div> 
+          }
+        </div>
+      </>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Enter a URL below to shorten
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Form />
       </header>
     </div>
   );
